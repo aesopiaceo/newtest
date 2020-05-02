62bd97a70924602622a59ff890603a69775beb27
eddombo <edwell@aesopia.co.za>
2020-05-02 16:15:30 +0200

    Added section6 of FlastTraining to inventory

---
+++ db.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 18:05:18 2020
+
+@author: eddomboAesopia
+"""
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
